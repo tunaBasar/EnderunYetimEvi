63ce6422e4654cf41683e5b18ae4faab1270c376
Tuna Basar <basartuna35@gmail.com>
2023-11-22 17:40:30 +0300

    Stundent Class defined

---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+namespace EnderunYetimEvi.Models
+{
+    public class Student
+    {
+
+        public String? Name { get; set; }=String.Empty;
+        public  String? LastName { get; set; }=String.Empty;
+        public String? FullName => $"{Name} {LastName?.ToUpper()}";
+        public int TC { get; set; }
+        public int Age { get; set; }
+        public DateTime ApplyAt { get; set; }
+
+        public Student()
+        {
+            ApplyAt=DateTime.Now; 
+        }
+    }
+
+
+
+
+
+}
+
+   
